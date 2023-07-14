aeb3d65b811598185319dc1afe132753a849b7e8
Leah Gunn <lcgunn@wisc.edu>
2023-07-14 13:53:53 -0500

    Calculating fuel by mass

---
+++ Fuel_By_Mass.py
@@ -0,0 +1,10 @@
+# Opens file of inputs and creates a list of ints
+    #s.strip() gets rid of white spaces
+
+with open("Day_1_Input", 'r') as input:
+    puzzle_input = [int(s.strip()) for s in input] 
+
+def launch_fuel(puzzle_input: int):
+    fuel: int = 0
+    for mass in puzzle_input:
+        fuel = (mass // 3) - 2
