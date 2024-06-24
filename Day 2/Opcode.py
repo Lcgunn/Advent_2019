a62d8ab9b20ae17854cb71a5d8eb09a384236e3d
Lcgunn <118074921+Lcgunn@users.noreply.github.com>
2024-06-24 10:26:37 -0400

    Comment formatting

--- Day 2/Opcode.py
+++ Day 2/Opcode.py
@@ -3,18 +3,21 @@
 with open("Day_2_Input", 'r') as input:
     data = input.read()
 
-puzzle_input = [int(i) for i in data.split(',')]    # Creates an array of ints based off of the list: data. Creates a new array element when it sees a ','
+ # Creates an array of ints based off of the list: data. Creates a new array element when it sees a ','
+puzzle_input = [int(i) for i in data.split(',')]
 
 current_op: int = 0
 index: int = 0
 
 while current_op != 99:     # If opcode == 99 then stop
     current_op = puzzle_input[index]
-    # If op code is 1, add the numbers located at the position value of the next two ints and put it in the address of the third next int
+    # If op code is 1, add the numbers located at the position value of the next two ints 
+    # and put it in the address of the third next int
     if current_op == 1:
         puzzle_input[puzzle_input[index + 3]] = puzzle_input[puzzle_input[index + 1]] + puzzle_input[puzzle_input[index + 2]]
     else:
-        # If op code is 2, multiple the numbers located at the position value of the next two ints and put it in the address of the third next int
+        # If op code is 2, multiple the numbers located at the position value of the next
+        # two ints and put it in the address of the third next int
         if current_op == 2:
             puzzle_input[puzzle_input[index + 3]] = puzzle_input[puzzle_input[index + 1]] * puzzle_input[puzzle_input[index + 2]]
     
